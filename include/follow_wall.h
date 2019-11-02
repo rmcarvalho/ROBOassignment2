f926d830050be0c9595d192b53e4e0b01acf9508
rmcarvalho <up201503717@fe.up.pt>
2019-11-02 16:23:21 +0000

    Robot is wandering to find wall and can detect if it is aproaching the edge of the map

--- include/follow_wall.h
+++ include/follow_wall.h
@@ -7,17 +7,29 @@
 #include "ros/ros.h"
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+//#include <geometry_msgs/PoseWithCovariance.h>
+//#include <geometry_msgs/TwistWithCovariance.h>
 
 class FollowWall {
  private:
    ros::Subscriber sensor_sub;
    ros::Publisher velocity_pub;
+   ros::Subscriber odometry_sub;
 
-   static const float ideal_distance = 0.2; //ideal distance the center of the robot should stay from the wall (in meters)
+   static const float ideal_distance = 0.05; //ideal distance the center of the robot should stay from the wall (in meters)
    static const float movement_speed = 0.2;
+
+   float last_odometry_position[2];
+   float last_odometry_yaw;
+   static const float map_width = 10;
+   static const float map_height = 10;
+   bool firstOdometry;
  public:
     FollowWall(char**);
     void SensorHandler(const sensor_msgs::LaserScan& msg);
+    void OdometryHandler(const nav_msgs::Odometry& msg);
+    bool AtMapLimit(float);
 };
 
 #endif
--- src/follow_wall.cpp
+++ src/follow_wall.cpp
@@ -10,7 +10,7 @@ float deg2rad (float deg) {
 
 FollowWall::FollowWall(char** argv) {
     ros::NodeHandle nh;
-
+    firstOdometry = false;
     //publish on robot movement topic
     std::string speed_topic = std::string("/") + std::string(argv[1]) + std::string("/cmd_vel");
     velocity_pub = nh.advertise<geometry_msgs::Twist>(speed_topic, 1);
@@ -18,6 +18,10 @@ FollowWall::FollowWall(char** argv) {
     //subscribe laser topic
     std::string laser_topic = std::string("/") + std::string(argv[1]) + std::string("/") + std::string(argv[2]);
     sensor_sub = nh.subscribe(laser_topic.c_str(), 1, &FollowWall::SensorHandler, this);
+
+    //subscribe odometry topic
+    std::string odom_topic = std::string("/") + std::string(argv[1]) + std::string("/odom");
+    odometry_sub = nh.subscribe(odom_topic.c_str(), 1, &FollowWall::OdometryHandler, this);
 }
 
 void FollowWall::SensorHandler(const sensor_msgs::LaserScan& msg) {    
@@ -53,17 +57,50 @@ void FollowWall::SensorHandler(const sensor_msgs::LaserScan& msg) {
             } 
         }*/
     }
+    
+    bool isOnMapLimit = AtMapLimit(msg.range_max);
 
-
-    if(min_distance < msg.range_max) //found some obstacle
+    if(min_distance < msg.range_max && !isOnMapLimit) //found some obstacle
     {   
+        ROS_INFO_STREAM("ON WALL");
         float k = 25;
+
         vel_msg.linear.x = movement_speed;
 		vel_msg.angular.z = (-k * (std::sin(deg2rad(alpha)) - (min_distance - ideal_distance))) * vel_msg.linear.x;
+        ROS_INFO_STREAM(min_distance - ideal_distance);
     } else { //no walls -> wander to find wall
-        vel_msg.linear.x = 0.3;
-		vel_msg.angular.z = 0.1;
+        if(isOnMapLimit) {
+            ROS_INFO_STREAM("Map LIMIT");
+            vel_msg.linear.x = 0;
+            vel_msg.angular.z = deg2rad(rand() % 180 + 90); // random
+        } else {
+            vel_msg.linear.x = movement_speed*2;
+		    vel_msg.angular.z = 0;
+        }
+        
     }
 
     velocity_pub.publish(vel_msg);
+}
+
+void FollowWall::OdometryHandler(const nav_msgs::Odometry& msg) {
+    last_odometry_position[0] = msg.pose.pose.position.x;
+    last_odometry_position[1] = msg.pose.pose.position.y;
+    geometry_msgs::Quaternion q = msg.pose.pose.orientation;
+
+    // yaw (z-axis rotation)
+    double siny_cosp = 2 * (q.w * q.z + q.x * q.y);
+    double cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z);
+    last_odometry_yaw = std::atan2(siny_cosp, cosy_cosp);
+    firstOdometry = true;
+}
+
+bool FollowWall::AtMapLimit(float range_max) {
+    float min_distance_to_edge = range_max + 0.005;
+    std::cout << firstOdometry << "\tat: " << last_odometry_position[0] << ", " << last_odometry_position[1] << "\trotated: " << last_odometry_yaw << "\tkeep distance: " << min_distance_to_edge << std::endl;
+    return firstOdometry && ((last_odometry_position[0] <= min_distance_to_edge && std::abs(rad2deg(last_odometry_yaw)) > 90) ||
+        (last_odometry_position[0] >= map_width - min_distance_to_edge && std::abs(rad2deg(last_odometry_yaw)) < 90) ||
+        (last_odometry_position[1] <= min_distance_to_edge && std::sin(last_odometry_yaw) < std::sin(deg2rad(10))) ||
+        (last_odometry_position[1] >= map_height - min_distance_to_edge && std::sin(last_odometry_yaw) > std::sin(deg2rad(-10))));
+
 }
